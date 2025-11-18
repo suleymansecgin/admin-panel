033b7db52aa1ccb2fd8299830c5a5406b26238ac
suleymansecgin <suleymansecginn@gmail.com>
2025-11-19 01:35:57 +0300

    Fix: schema.sql dosyasına açıklamalar eklendi

--- admin_panel/src/main/resources/schema.sql
+++ admin_panel/src/main/resources/schema.sql
@@ -1,3 +1,11 @@
 -- Admin Panel Schema Oluşturma
 CREATE SCHEMA IF NOT EXISTS admin_panel;
 
+-- Schema'ya yetki verme (eğer admin_user kullanıcısı varsa)
+-- GRANT ALL ON SCHEMA admin_panel TO admin_user;
+-- ALTER DEFAULT PRIVILEGES IN SCHEMA admin_panel GRANT ALL ON TABLES TO admin_user;
+-- ALTER DEFAULT PRIVILEGES IN SCHEMA admin_panel GRANT ALL ON SEQUENCES TO admin_user;
+
+-- Not: Tablolar ve index'ler Hibernate tarafından otomatik oluşturulacak (ddl-auto=update)
+-- Eğer manuel index oluşturmak isterseniz, aşağıdaki gibi IF NOT EXISTS kullanın:
+-- CREATE INDEX IF NOT EXISTS idx_tarih ON admin_panel.siparisler (siparis_tarihi);
